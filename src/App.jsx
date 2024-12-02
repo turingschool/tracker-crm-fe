ec986c6c3ded241e40071c444f794dfabf2fbb2f
Erin Pintozzi <epintozzi@turing.edu>
2024-12-01 23:00:28 -0600

    login set up

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import './App.css';
+import { useState } from 'react';
+import LoginForm from './Login';
+import { getUser } from './apiCalls';
+
+function App() {
+
+  const [userId, setUserId] = useState(null);
+  const [userData, setUserData] = useState({});
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+
+  const handleLogin = async (id) => {
+    try {
+      const loginResponse = await getUser(id);
+      if (loginResponse) {
+        setUserData({
+          id: loginResponse.data.id,
+          email: loginResponse.data.attributes.email,
+          username: loginResponse.data.attributes.username,
+        });
+        setUserId(loginResponse.data.id);
+        setIsLoggedIn(true);
+      }
+    } catch (err) {
+      console.error('Error fetching logged in user:', err);
+      setIsLoggedIn(false);
+    }
+  };  
+
+  const handleLogout = () => {
+    setUserId(null);
+    setUserData({});
+    setIsLoggedIn(false);
+  };
+
+  const userIsLoggedIn = () => {
+    setIsLoggedIn(true);
+  };
+
+  const userLogOut = () => {
+    setIsLoggedIn(false);
+    setUserData({});
+  };
+
+
+  return (
+    <div>
+      {!isLoggedIn && (
+        <>
+          <h1>Please login</h1>
+          <LoginForm onLogin={handleLogin} />
+        </>
+      )}
+      {isLoggedIn && (
+        <>
+          <h1>Welcome, {userData.username}</h1>
+          <button onClick={handleLogout}>Log Out</button>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
