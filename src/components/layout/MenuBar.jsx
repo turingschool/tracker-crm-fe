95430c7b99d5edde180b83a1799dc8b80cac0016
ROlearyPro <rolearyprogramming@gmail.com>
2024-12-04 16:40:25 -0700

    test commit because of errors

--- src/components/layout/MenuBar.jsx
+++ src/components/layout/MenuBar.jsx
@@ -1,23 +1,40 @@
 import './MenuBar.css';
-import home from '../../components/icons/home_738822.png'
-import turing from '../../components/icons/turing-logo-gray.png'
-import person from '../../components/icons/person-icon.png'
-import building from '../../components/icons/building-1062.png'
-import plus from '../../components/icons/plus-small_4338829.png'
-import papers from '../../components/icons/documents-symbol_35920.png'
+// import home from '../../components/icons/home_738822.png'
+// import turing from '../../components/icons/turing-logo-gray.png'
+// import person from '../../components/icons/person-icon.png'
+// import building from '../../components/icons/building-1062.png'
+// import plus from '../../components/icons/plus-small_4338829.png'
+// import papers from '../../components/icons/documents-symbol_35920.png'
 function MenuBar() {
   return (
     <header>
-      <div className='logo-container'>
-        <img className="icon" src={turing} />
-        <img className="small-icon" src={home} />
-        <img className="small-icon" src={person} />
-        <img className="small-icon" src={building} />
-        <img className="small-icon" src={papers} />
-        <img className="small-icon" src={plus} />
+      <div className='flex'>
+        {/* <img className="icon"
+          // onClick={() => Navigate()} 
+          src={turing} />
+        <img className="small-icon"
+          // onClick={() => Navigate()} 
+          src={home} />
+        <img className="small-icon"
+          // onClick={() => Navigate()} 
+          src={person} />
+        <img className="small-icon"
+          // onClick={() => Navigate()} 
+          src={building} />
+        <img className="small-icon"
+          // onClick={() => Navigate()} 
+          src={papers} />
+        <img className="small-icon"
+          // onClick={() => Navigate()} 
+          src={plus} />
         <div className='account-icon-container'>
           <img className="account-icon" src={person} />
-        </div>
+        </div> */}
+
+        <p className='text-green-500'>
+          blah blah test
+
+        </p>
       </div>
     </header >
   );
