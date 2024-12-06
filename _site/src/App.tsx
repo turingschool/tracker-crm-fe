04d064e5f05e3a70f8dc8957fceff53e21b0e35e
ROlearyPro <rolearyprogramming@gmail.com>
2024-12-06 13:01:41 -0700

    Pushing without testing, as .ts doesn't seem to work with cypress for some reason?

---
+++ _site/src/App.tsx
@@ -0,0 +1,74 @@
+import './App.css';
+import { useState } from 'react';
+import LoginForm from './Login.tsx';
+import { getUser } from './apiCalls.tsx';
+import MenuBar from './components/layout/MenuBar.tsx'
+
+interface UserInfo {
+  id: number,
+  username: string,
+  email: string
+}
+
+
+function App() {
+
+  const [userId, setUserId] = useState(null);
+  const [userData, setUserData] = useState< Partial < UserInfo >> ({});
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = async (id: number) => {
+    try {
+      const loginResponse = await getUser(id);
+      if (loginResponse) {
+        setUserData({
+          id: loginResponse.data.id,
+          email: loginResponse.data.attributes.email,
+          username: loginResponse.data.attributes.username,
+        });
+        setUserId(loginResponse.data.id);
+        setIsLoggedIn(true);
+      }
+    } catch (err) {
+      console.error('Error fetching logged in user:', err);
+      setIsLoggedIn(false);
+    }
+  };  
+
+  const handleLogout = () => {
+    setUserId(null);
+    setUserData({});
+    setIsLoggedIn(false);
+  };
+
+  // const userIsLoggedIn = () => {
+  //   setIsLoggedIn(true);
+  // };
+
+  // const userLogOut = () => {
+  //   setIsLoggedIn(false);
+  //   setUserData({});
+  // };
+
+
+  console.log(`we need to have ${userId}... NOT`)
+  return (
+    <div>
+      {!isLoggedIn && (
+        <>
+          <h1>Please login</h1>
+          <LoginForm onLogin={handleLogin} />
+        </>
+      )}
+      {isLoggedIn && (
+        <div className='flex flex-row'>
+        <MenuBar />
+          <h1>Welcome, {userData.username}</h1>
+          <button onClick={handleLogout}>Log Out</button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
