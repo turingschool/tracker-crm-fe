fb21d6faffe141db0db88b9060091a0fba8dfcc7
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-12-13 16:11:28 -0700

    Resolved extra BrowserRouter in index

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Routes, Route } from 'react-router-dom';
 // import turingLogo from './Turing-logo.png';
 import './App.css';
 import { useState } from 'react';
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<BrowserRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
