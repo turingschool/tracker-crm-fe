ac3314d5dda00984b77e1ca52ff53bd4ab5a4f0f
Candice Cirbo <thecirbos@icloud.com>
2024-12-17 15:40:22 -0700

    Test: adds fixture file for contacts

---
+++ cypress/fixtures/mockContactsData.js
@@ -0,0 +1,34 @@
+
+
+const mockContactsData = {
+  "data": [
+    {
+      "id": "1",
+      "type": "contacts",
+      "attributes": {
+          "first_name": "John",
+          "last_name": "Smith",
+          "company_id": 1,
+          "email": "123@example.com",
+          "phone_number": "123-555-6789",
+          "notes": "Type notes here...",
+          "user_id": 4
+      }
+    },
+    {
+      "id": "2",
+      "type": "contacts",
+      "attributes": {
+          "first_name": "Jane",
+          "last_name": "Smith",
+          "company_id": 1,
+          "email": "123@example.com",
+          "phone_number": "555-555-6789",
+          "notes": "Type notes here...",
+          "user_id": 4
+      }
+    }
+  ]   
+}
+
+export { mockContactsData };
