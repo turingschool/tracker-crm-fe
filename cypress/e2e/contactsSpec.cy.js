112a31e49cf410c0b05efb32db7f4792394f5489
Renee Messersmith <morganrmessersmith@gmail.com>
2024-12-17 17:32:57 -0500

    Feat: company name might be working

--- cypress/e2e/contactsSpec.cy.js
+++ cypress/e2e/contactsSpec.cy.js
@@ -1,22 +1,32 @@
-// describe('template spec', () => {
-//   it('passes', () => {
-//     cy.visit('https://example.cypress.io')
+import { mockContactsData } from "../fixtures/mockContactsData";
+
+// describe("Navigation to Contacts", () => {
+//   beforeEach(() => {
+//     cy.intercept("GET", "http://localhost:3001/api/v1/users/2/companies", {
+//       statusCode: 200,
+//       body: mockCompanies,
+//       headers: {
+//         "Content-Type": "application/json"
+//       }
+//     }).as("getCompanies");
+    
+//     cy.visit("http://localhost:3000/");
+//     cy.get("img[alt='Companies']").click();
 //   })
-// })
+// });
 
 
 describe("Contacts page", () => {
   beforeEach(() => {
-    // cy.intercept("GET", "http://localhost:3001/api/v1/users/4/contacts", {
-    //   statusCode: 200,
-    //   body: mockCompanies,
-    //   headers: {
-    //     "Content-Type": "application/json"
-    //   }
-    // }).as("getCompanies");
+    cy.intercept("GET", "http://localhost:3001/api/v1/users/4/contacts", {
+      statusCode: 200,
+      body: mockContactsData,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).as("get-contacts");
     
     cy.visit("http://localhost:3000/contacts");
-    // cy.get("img[alt='Contacts']").click();
   });
 
   it ("Should have a header with the text 'Contacts'", () => {
@@ -26,17 +36,54 @@ describe("Contacts page", () => {
   it ("Should have a search bar", () => {
     cy.get("input[type='search']").should("exist");
     cy.get("input[type='search']").should("have.attr", "placeholder", "Search Contacts...");
-  })
+  });
 
   it ("Should have a button with the text 'Add Contact +'", () => {
     cy.contains("Add New").click();
-    cy.url().should("include", "/newContacts");
-  })
+    cy.url().should("include", "/contacts/new");
+  });
   
   it("Should display the correct table headers", () => {
     cy.get("table").find("th").should("have.length", 3);
-    cy.get("table").find("th").eq(0).should("have.text", "Company Name");
-    cy.get("table").find("th").eq(1).should("have.text", "Application Status"); 
+    cy.get("table").find("th").eq(0).should("have.text", "Name");
+    cy.get("table").find("th").eq(1).should("have.text", "Company"); 
     cy.get("table").find("th").eq(2).should("have.text", "Notes");
+  });
+
+  it("Should display the correct number of companies", () => {
+    cy.wait("@get-contacts");
+    cy.get("table").find("tr").should("have.length", 3);
+  });
+
+  it("Should display the correct company information", () => {
+    cy.wait("@get-contacts");
+    cy.get("table tbody tr").eq(0).find("td").eq(0).should("have.text", "John Smith");
+    cy.get("table tbody tr").eq(0).find("td").eq(1).should("have.text", "Company Name");
+    cy.get("table tbody tr").eq(0).find("td").eq(2).should("have.text", "Type notes here...");
+    cy.get("table tbody tr").eq(1).find("td").eq(0).should("have.text", "Jane Smith");
+    cy.get("table tbody tr").eq(1).find("td").eq(1).should("have.text", "Company Name");
+    cy.get("table tbody tr").eq(1).find("td").eq(2).should("have.text", "Type notes here...");
+  });
+
+  it("Should search for contacts by first name", () => {
+
+  });
+
+  it("Should display 'No contacts found' when no contacts exist", () => {
+    cy.intercept("GET", "http://localhost:3001/api/v1/users/4/contacts", {
+      statusCode: 200,
+      body: { data: [] },
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).as("empty-contacts");
+  
+    cy.reload(); 
+    cy.wait("@empty-contacts");
+  
+    cy.get("table").find("th").should("have.length", 3);
+    cy.get("table tbody tr").eq(0).find("td").eq(0).should("not.exist");
+    // cy.get("[data-testid='no-companies']").should("exist").and("have.text", "No companies found");
+    // cy.get("table").should("not.exist");
   });
 })
--- src/components/contacts/Contacts.tsx
+++ src/components/contacts/Contacts.tsx
@@ -16,15 +16,33 @@ interface ContactData {
   }
 };
 
+interface CompanyAttributes {
+  id: number;
+  name: string;
+  website: string;
+  street_address: string;
+  city: string;
+  state: string;
+  zip_code: string;
+  notes: string;
+}
+
+interface Company {
+  id: number;
+  type: string;
+  attributes: CompanyAttributes;
+}
+
 function Contacts() {
-  const [contacts, setContacts] = useState<ContactData[] | []>([])
-  const [allContacts, setAllContacts] = useState<ContactData[] | []>([])
-  const [contactSearch, setContactSearch] = useState<string>("")
+  const [contacts, setContacts] = useState<ContactData[] | []>([]);
+  const [allContacts, setAllContacts] = useState<ContactData[] | []>([]);
+  const [contactSearch, setContactSearch] = useState<string>("");
+  const [companies, setCompanies] = useState<Company[] | null>([]); 
   
   useEffect(() => {
     const fetchContacts = async () => {
       try {
-        const token = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0LCJleHAiOjE3MzQ0Njg1MjR9.P5tqsH3teuE_E8oPOM01SD_makjWlsuSUQ2unr5bmn0";
+        const token = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0LCJleHAiOjE3MzQ1NjA3ODN9.xJdO2fr_icpw5LqE9o3Oog8PJ6eyjiHE2qxx3y8wSfg";
         const response = await fetch("http://localhost:3001/api/v1/users/4/contacts", {
           method: "GET",
           headers: {
@@ -46,8 +64,35 @@ function Contacts() {
     fetchContacts();
   }, []);
 
+  useEffect(() => {
+    const fetchCompanies = async () => {
+      try {
+        const token =
+          "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0LCJleHAiOjE3MzQ1NjA3ODN9.xJdO2fr_icpw5LqE9o3Oog8PJ6eyjiHE2qxx3y8wSfg";
+        const response = await fetch("http://localhost:3001/api/v1/users/2/companies", {
+          method: "GET",
+          headers: {
+            authorization: `Bearer ${token}`,
+            "Content-Type": "application/json"
+          },
+        });
+        console.log(response)
+
+        if (!response.ok) {
+          throw new Error(`Failed to fetch companies: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        console.log(data.data);
+        setCompanies(data.data as Company[]);
+      } catch (error) {
+        console.error("Fetch error", error);
+      }
+    };
+    fetchCompanies();
+  }, []);
+
   function searchContactList(event: React.ChangeEvent<HTMLInputElement>) {
-  // function searchContactList(event: React.FormEvent<HTMLFormElement>) {
     const search = event.target.value
     setContactSearch(search);
     if (search === "") {
@@ -63,7 +108,6 @@ function Contacts() {
 
   const searchBar = (
     <form onSubmit={(event) => event.preventDefault()}>
-      {/* <img className='search-icon' src={searchIcon} alt='search icon'/> */}
       <input
         className='p-[1vh] border-2 border-slate-800 rounded w-[200px] h-full'
         type="search"
@@ -73,21 +117,57 @@ function Contacts() {
         onChange={searchContactList}
       />
     </form>
-  )
+  );
 
+  // const contactData = contacts.map(data => {
+  //   let contactCompanyName = "N/A";
+  //   if (data.attributes.company_id && companies) {
+  //     contactCompany = companies.find(company => {
+  //       console.log(company, "HERE")
+  //       return company.id === data.attributes.company_id
+  //     })
+  //     // contactCompany = companies.find(
+  //     //   company => company.id === data.attributes.company_id
+  //     // )?.name
+  //   } else {
+  //     contactCompany = "N/A";
+  //   }
+  //   return (
+  //    <tr key={data.id} className="even:bg-gray-50 hover:bg-gray-100">
+  //     <td className="p-4 border-b">{data.attributes.first_name} {data.attributes.last_name}</td>
+  //     <td className="p-4 border-b">
+  //       {contactCompany.attributes.name}
+  //     </td>
+  //     <td className="p-4 border-b">{data.attributes.notes}</td>
+  //   </tr>
+  //   )
+  // });
   const contactData = contacts.map(data => {
-    console.log(data, "DATA")
+    // Default to "N/A" if no company is found
+    let contactCompanyName = "N/A";
+  
+    // Find the company if company_id exists and companies array is not null
+    if (data.attributes.company_id && companies) {
+      const foundCompany = companies.find(
+        company => company.id === data.attributes.company_id
+      );
+      if (foundCompany) {
+        contactCompanyName = foundCompany.attributes.name;
+      }
+    }
+  
     return (
-
-      // <tr key={data.id} className="bg-gray-50 hover:bg-gray-100">
-     <tr key={data.id} className="even:bg-gray-50 hover:bg-gray-100">
-        <td className="p-4 border-b">{data.attributes.first_name} {data.attributes.last_name}</td>
-        <td className="p-4 border-b">Company Name</td>
+      <tr key={data.id} className="even:bg-gray-50 hover:bg-gray-100">
+        <td className="p-4 border-b">
+          {data.attributes.first_name} {data.attributes.last_name}
+        </td>
+        <td className="p-4 border-b">
+          {contactCompanyName} {/* Safely display the company name or "N/A" */}
+        </td>
         <td className="p-4 border-b">{data.attributes.notes}</td>
       </tr>
-    )
-  })
-
+    );
+  });
 
   return (
     <section className='flex'>
@@ -95,14 +175,6 @@ function Contacts() {
       <div className='w-[70vw] pl-[4vw]'>
         <h1 className="text-[5vh] font-bold text-cyan-600 my-[5vh]">Contacts</h1>
         <div className='flex justify-between items-center'>
-          {/* <input
-          onSubmit={(event) => event.preventDefault()}
-          className='p-[1vh] border-2 border-slate-800 rounded w-[200px] h-full'
-          // className='p-[1vh] border-2 border-slate-800 rounded w-[12vw] h-full'
-          type='text'
-          // value={}
-          placeholder='Search contacts...'
-          /> */}
           {searchBar}
           <Link to='/contacts/new'>
             <button className='bg-cyan-600 text-white p-[1vh] rounded w-[10vw]'>Add New +</button>
