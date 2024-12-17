20614b63ac3e0b2b20f2a28c5b1419d0e31d3fbf
Marshall Hotaling <marshall.hotaling@gmail.com>
2024-12-17 15:30:13 -0700

    rough draft of API call
    got the end point for the api call

--- src/Interfaces.tsx
+++ src/Interfaces.tsx
@@ -1,35 +1,23 @@
 export interface UserData {
-  token: string,
-  user: {
-    data: {
-      id: number,
-      type: string,
-      attributes: {
-        name: string,
-        email: string,
-        companies: []
-      }
-    }
-  }
+    token: string,
+    user: {
+        id: number,
+        type: string,
+        attributes: {
+            name: string,
+            email: string,
+            companies: []
+        }
+    },
+    Userdata?: [() => void, UserData]
+}
+
+export interface LoginFormProps {
+    setLogin: (login: boolean) => void;
+    setData: (data: UserData) => void;
+    setId: (id: number) => void;
 }
 
 export interface UserInformationProps {
-  userData?: Partial<UserData>;
-}
-
-export interface CompanyAttributes {
-  id: number;
-  name: string;
-  website: string;
-  street_address: string;
-  city: string;
-  state: string;
-  zip_code: string;
-  notes: string;
-}
-
-export interface Company {
-  id: number;
-  type: string;
-  attributes: CompanyAttributes;
+    userData?: Partial<UserData>;
 }
