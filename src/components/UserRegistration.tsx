68212b533d19a0bd7f410b011dc4c53b0fb62061
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-12-17 18:57:39 -0700

    Save just in case

--- src/components/UserRegistration.tsx
+++ src/components/UserRegistration.tsx
@@ -42,55 +42,57 @@ function UserRegistration(): React.ReactElement {
   return (
     <div className="login-form-wrap flex w-screen h-screen justify-center">
       <div className="form-inputs w-6/12 flex">
-        <img className='turing-logo size-20' src={turingLogo} />
-        
-
+        <img className='turing-logo size-20 mt-7 ml-7' src={turingLogo} />
 
         <form onSubmit={handleFormSubmit}>
-          <h2 className="text-3xl font-bold text-center">Create Account</h2>
-          <label htmlFor="name" className='block text-x1 font-[Helvetica Neue] font-sans text-xl '>Name</label>
-          <input
-            id="name"
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-            placeholder="Add text"
-            className='p-[8px] mt-[4px] border-4 rounded-md border-slate-600 bg-slate-200 py-[2vh] px-[3vh]'
-            />
 
-          <label htmlFor="email" className='block text-x1 font-[Helvetica Neue] font-sans text-xl '>Email</label>
-          <input
-            id="email"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            placeholder="Add text"
-            className='p-[8px] mt-[4px] border-4 rounded-md border-slate-600 bg-slate-200 py-[2vh] px-[3vh]'
-            />
+          <h2 className="text-4xl text-gray-600 font-[Helvetica Neue] text-center mt-40 mb-10 tracking-wider">Create Account</h2>
 
-          <label htmlFor="password" className='block text-x1 font-[Helvetica Neue] font-sans text-xl '>Password</label>
-          <input
-            id="password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            placeholder="Add text"
-            className='p-[8px] mt-[4px] border-4 rounded-md border-slate-600 bg-slate-200 py-[2vh] px-[3vh]'
-            />
+          <div className="">
+            <label htmlFor="name" className="block font-light text-gray-500 font-[Helvetica Neue] font-sans text-xl tracking-wider">Name</label>
+            <input
+              id="name"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+              placeholder="Add text"
+              className="p-[8px] mt-[4px] mb-5 border-4 rounded-md border-slate-600 bg-slate-200 py-[2vh] px-[3vh]"
+              />
 
-          <label htmlFor="passwordConfirmation" className='block text-x1 font-[Helvetica Neue] font-sans text-xl '>Confirm Password</label>
-          <input
-            id="passwordConfirmation"
-            type="password"
-            value={passwordConfirmation}
-            onChange={(e) => setPasswordConfirmation(e.target.value)}
-            required
-            placeholder="Add text"
-            className='p-[8px] mt-[4px] border-4 rounded-md border-slate-600 bg-slate-200 py-[2vh] px-[3vh]'
-          />
+            <label htmlFor="email" className="block font-light text-gray-500 font-[Helvetica Neue] font-sans text-xl tracking-wider">Email</label>
+            <input
+              id="email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              placeholder="Add text"
+              className="p-[8px] mt-[4px] mb-5 border-4 rounded-md border-slate-600 bg-slate-200 py-[2vh] px-[3vh]"
+              />
+
+            <label htmlFor="password" className="block font-light text-gray-500 font-[Helvetica Neue] font-sans text-xl tracking-wider">Password</label>
+            <input
+              id="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              placeholder="Add text"
+              className="p-[8px] mt-[4px] mb-5 border-4 rounded-md border-slate-600 bg-slate-200 py-[2vh] px-[3vh]"
+              />
+
+            <label htmlFor="passwordConfirmation" className="block font-light text-gray-500 font-[Helvetica Neue] font-sans text-xl tracking-wider">Confirm Password</label>
+            <input
+              id="passwordConfirmation"
+              type="password"
+              value={passwordConfirmation}
+              onChange={(e) => setPasswordConfirmation(e.target.value)}
+              required
+              placeholder="Add text"
+              className="p-[8px] mt-[4px] border-4 rounded-md border-slate-600 bg-slate-200 py-[2vh] px-[3vh]"
+            />
+          </div>
 
           {errorMessage && <p className="error-message">{errorMessage}</p>}
 
@@ -101,25 +103,27 @@ function UserRegistration(): React.ReactElement {
               >
                 Register
             </button>
-              <p className="text-gray-500 text-2xl font-extralight block mt-4 text-center">
-                Already have an account? <Link to="/login" className="hover:underline">Click here to login.</Link>
+              <p className="text-gray-500 text-xl font-extralight block mt-4 text-center">
+                Already have an account? 
+                <br/>
+                <Link to="/login" className="hover:underline">Click here to login.</Link>
               </p>
             </div>
         </form>
       </div>
 
-      <div className='quad-color-bar flex flex-col w-[1%] h-[100%]'>
-        <div className='cyan-bar bg-cyan-500 w-[100%] h-[25%]'/>
-        <div className='yellow-bar bg-yellow-500 w-[100%] h-[25%]'/>
-        <div className='red-bar bg-red-500 w-[100%] h-[25%]'/>
-        <div className='green-bar bg-green-500 w-[100%] h-[25%]'/>
+      <div className="quad-color-bar flex flex-col w-[1%] h-[100%]">
+        <div className="cyan-bar bg-cyan-500 w-[100%] h-[25%]"/>
+        <div className="yellow-bar bg-yellow-500 w-[100%] h-[25%]"/>
+        <div className="red-bar bg-red-500 w-[100%] h-[25%]"/>
+        <div className="green-bar bg-green-500 w-[100%] h-[25%]"/>
       </div>
-      <div className='title-wrap w-6/12 flex flex-col justify-center items-center bg-cyan-800'>
-        <div className='app-name-and-author flex justify-center'>
-          <h1 className='app-name font-sans font-[Helvetica Neue] text-7xl font-semibold tracking-wide text-slate-100'>Tracker</h1>
-          <h2 className='app-author font-sans font-[Helvetica Neue] text-3xl font-bold leading-9 tracking-wide text-cyan-600'><br />&nbsp; by Turing</h2>
+      <div className="title-wrap w-6/12 flex flex-col justify-center items-center bg-cyan-800">
+        <div className="app-name-and-author flex justify-center">
+          <h1 className="app-name font-sans font-[Helvetica Neue] text-7xl font-semibold tracking-wide text-sky-200">Tracker</h1>
+          <h2 className="app-author font-sans font-[Helvetica Neue] text-3xl font-bold leading-9 tracking-wide text-cyan-600"><br />&nbsp; by Turing</h2>
         </div>
-        <h3 className='app-tagline flex justify-center font-sans font-[Helvetica Neue] text-3xl font-extralight tracking-widest text-slate-100'>Job hunting made easier.</h3>
+        <h3 className="app-tagline flex justify-center font-sans font-[Helvetica Neue] text-3xl font-extralight tracking-widest text-sky-200">Job hunting made easier.</h3>
       </div>
       
     </div>
