f493b8891a09bc31f01a0b440dcf20411fe8d50a
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-12-18 16:17:33 -0700

    Refactor: match styling to mock

--- src/components/job_applications/NewJobApplication.tsx
+++ src/components/job_applications/NewJobApplication.tsx
@@ -17,99 +17,108 @@ function NewJobApplication() {
       <MenuBar />
       <div className='flex-1 p-5'>
         <h1 className="text-[2.5vw] font-[Helvetica Neue] font-semibold text-cyan-600">Add New Application</h1>
-        <form className='position-right'>
+        <form className="grid grid-cols-2 gap-4">
 
-          {/* Position Title*/}
-          <label className="text-[1vw] font-[Helvetica Neue]">
-            <span className="font-semibold">Position Title:</span>
-            <input
-              type="text"
-              value={positionTitle}
-              onChange={(e) => setPositionTitle(e.target.value)}
-              className="p-2 border border-black-800 rounded-lg focus:outline-none focus:ring-2 focus:ring-black-800"
-            />
-          </label>
+          <div className='LEFT BLOCK'>
 
-          {/* Company*/}
-          <label className="text-[1vw] font-[Helvetica Neue]">
-            <span className="font-semibold">Company:</span>
-            <input
-              type="number"
-              value={dateApplied}
-              onChange={(e) => setDateApplied(e.target.value)}
-              className="p-2 border border-black-800 rounded-lg focus:outline-none focus:ring-2 focus:ring-black-800"
-            />
-          </label>
+            {/* Position Title*/}
+            <label className="text-[1vw] font-[Helvetica Neue] flex flex-col w-[90%]">
+              <span className="font-semibold">Position Title:</span>
+              <input
+                type="text"
+                value={positionTitle}
+                onChange={(e) => setPositionTitle(e.target.value)}
+                className="p-2 border-4 border-slate-800 rounded-lg focus:outline-none focus:ring-2 m-2"
+              />
+            </label>
+
+            {/* Company*/}
+            <label className="text-[1vw] font-[Helvetica Neue] flex flex-col w-[90%]">
+              <span className="font-semibold">Company:</span>
+              <input
+                type="number"
+                value={dateApplied}
+                onChange={(e) => setDateApplied(e.target.value)}
+                className="p-2 border-4 border-slate-800 rounded-lg focus:outline-none focus:ring-2 m-2"
+              />
+            </label>
+
+            <div className='flex flex-row' >
+              {/* Date Applied */}
+              <label className="text-[1vw] font-[Helvetica Neue] flex flex-col w-[45%]">
+                <span className="font-semibold">Date Applied:</span>
+                <input
+                  type="date"
+                  value={dateApplied}
+                  onChange={(e) => setDateApplied(e.target.value)}
+                  className="p-2 border-4 border-slate-800 rounded-lg focus:outline-none focus:ring-2 m-2"
+                />
+              </label>
+
+              {/* Status */}
+              <label className="text-[1vw] font-[Helvetica Neue] flex flex-col w-[45%]">
+                <span className="font-semibold">Application Status:</span>
+                <input
+                  type="number"
+                  value={status}
+                  onChange={(e) => setStatus(Number(e.target.value))}
+                  className="p-2 border-4 border-slate-800 rounded-lg focus:outline-none focus:ring-2 m-2"
+                />
+              </label>
+            </div>
 
 
-          {/* Date Applied */}
-          <label className="text-[1vw] font-[Helvetica Neue]">
-            <span className="font-semibold">Date Applied:</span>
-            <input
-              type="date"
-              value={dateApplied}
-              onChange={(e) => setDateApplied(e.target.value)}
-              className="p-2 border border-black-800 rounded-lg focus:outline-none focus:ring-2 focus:ring-black-800"
-            />
-          </label>
+            {/* Job Description */}
+            <label className="text-[1vw] font-[Helvetica Neue] flex flex-col w-[90%]">
+              <span className="font-semibold">Job Description:</span>
+              <textarea
+                value={jobDescription}
+                onChange={(e) => setJobDescription(e.target.value)}
+                className="p-2 border-4 border-slate-800 rounded-lg focus:outline-none focus:ring-2  m-2"
+                rows={6}
+              />
+            </label>
 
-          {/* Status */}
-          <label className="text-[1vw] font-[Helvetica Neue]">
-            <span className="font-semibold">Application Status:</span>
-            <input
-              type="number"
-              value={status}
-              onChange={(e) => setStatus(Number(e.target.value))}
-              className="p-2 border border-black-800 rounded-lg focus:outline-none focus:ring-2 focus:ring-black-800"
-            />
-          </label>
+            {/* Contact Information */}
+            <label className="text-[1vw] font-[Helvetica Neue] flex flex-col w-[90%]">
+              <span className="font-semibold">Contact Information:</span>
+              <input
+                type="text"
+                value={contactInformation}
+                onChange={(e) => setContactInformation(e.target.value)}
+                className="p-2 border-4 border-slate-800 rounded-lg focus:outline-none focus:ring-2 m-2"
+              />
+            </label>
+          </div>
 
-          {/* Notes */}
-          <label className="text-[1vw] font-[Helvetica Neue]">
-            <span className="font-semibold">Notes:</span>
-            <textarea
-              value={notes}
-              onChange={(e) => setNotes(e.target.value)}
-              className="p-2 border border-black-800 rounded-lg focus:outline-none focus:ring-2 focus:ring-black-800 w-full"
-              rows={3}
-            />
-          </label>
+          <div className='m-2'>
 
-          {/* Application URL */}
-          <label className="text-[1vw] font-[Helvetica Neue]">
-            <span className="font-semibold">Application URL:</span>
-            <input
-              type="url"
-              value={applicationURL}
-              onChange={(e) => setApplicationURL(e.target.value)}
-              className="p-2 border border-black-800 rounded-lg focus:outline-none focus:ring-2 focus:ring-black-800"
-            />
-          </label>
+            {/* Application URL */}
+            <label className="text-[1vw] font-[Helvetica Neue] flex flex-col">
+              <span className="font-semibold">Application URL:</span>
+              <input
+                type="url"
+                value={applicationURL}
+                onChange={(e) => setApplicationURL(e.target.value)}
+                className="p-2 border-4 border-slate-800 rounded-lg focus:outline-none focus:ring-2 m-2 w-[90%]"
+              />
+            </label>
 
-          {/* Contact Information */}
-          <label className="text-[1vw] font-[Helvetica Neue]">
-            <span className="font-semibold">Contact Information:</span>
-            <input
-              type="text"
-              value={contactInformation}
-              onChange={(e) => setContactInformation(e.target.value)}
-              className="p-2 border border-black-800 rounded-lg focus:outline-none focus:ring-2 focus:ring-black-800"
-            />
-          </label>
-
-          {/* Job Description */}
-          <label className="text-[1vw] font-[Helvetica Neue]">
-            <span className="font-semibold">Job Description:</span>
-            <textarea
-              value={jobDescription}
-              onChange={(e) => setJobDescription(e.target.value)}
-              className="p-2 border border-black-800 rounded-lg focus:outline-none focus:ring-2 focus:ring-black-800 w-full"
-              rows={3}
-            />
-          </label>
-
+            {/* Notes */}
+            <label className="text-[1vw] font-[Helvetica Neue] flex flex-col">
+              <span className="font-semibold">Notes:</span>
+              <textarea
+                value={notes}
+                onChange={(e) => setNotes(e.target.value)}
+                className="p-2 border-4 border-slate-800 rounded-lg focus:outline-none focus:ring-2 w-[90%] m-2"
+                rows={15}
+              />
+            </label>
+          </div>
         </form>
-        <button className="text-[1vw] font-[Helvetica Neue] text-white bg-cyan-600 pl-11 pr-11 p-3 rounded-md">Save</button>
+        <div className='pt-4 pl-2'>
+        <button className="text-[1vw] font-[Helvetica Neue] text-white bg-cyan-600 pl-11 pr-11 p-3 rounded-md hover:bg-cyan-800">Save</button>
+        </div>
       </div>
     </div>
   )
