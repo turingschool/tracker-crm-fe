d4c050d3509879e5427ccf8c4b216014171c7b6b
Stefan Bloom <stefanjbloom88@gmail.com>
2024-12-19 15:58:28 -0700

    Feat: fetch in ShowContact and added Link to contact name

--- src/components/contacts/Contacts.tsx
+++ src/components/contacts/Contacts.tsx
@@ -3,27 +3,28 @@ import { Link } from "react-router-dom";
 import { useUserLoggedContext } from "../../context/UserLoggedContext";
 
 interface ContactData {
-  "id": string,
-  "type": string,
-  "attributes": {
-      "first_name": string,
-      "last_name": string,
-      "company_id": number,
-      "email": string,
-      "phone_number": string,
-      "notes": string,
-      "user_id": number,
-      "company": {
-        "id": number;
-        "name": string;
-        "website": string;
-        "street_address": string;
-        "city": string;
-        "state": string;
-        "zip_code": string;
-        "notes": string;}
-  }
-};
+  id: string;
+  type: string;
+  attributes: {
+    first_name: string;
+    last_name: string;
+    company_id: number;
+    email: string;
+    phone_number: string;
+    notes: string;
+    user_id: number;
+    company: {
+      id: number;
+      name: string;
+      website: string;
+      street_address: string;
+      city: string;
+      state: string;
+      zip_code: string;
+      notes: string;
+    };
+  };
+}
 
 function Contacts() {
   const [contacts, setContacts] = useState<ContactData[] | []>([]);
@@ -32,17 +33,20 @@ function Contacts() {
   const [fetchError, setFetchError] = useState<string | null>(null);
 
   const { token } = useUserLoggedContext();
-  
+
   useEffect(() => {
     const fetchContacts = async () => {
       try {
-        const response = await fetch("http://localhost:3001/api/v1/users/4/contacts", {
-          method: "GET",
-          headers: {
-            "Authorization": `Bearer ${token}`,
-            "Content-Type": "application/json"
-          },
-        });
+        const response = await fetch(
+          "http://localhost:3001/api/v1/users/4/contacts",
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
+            },
+          }
+        );
         if (!response.ok) {
           throw new Error(`Failed to fetch contacts: ${response.statusText}`);
         }
@@ -50,7 +54,7 @@ function Contacts() {
         setContacts(data.data as ContactData[]);
         setAllContacts(data.data as ContactData[]);
       } catch (error) {
-        setFetchError(`${(error as Error).message}. Please try again later.`)
+        setFetchError(`${(error as Error).message}. Please try again later.`);
         console.error("Fetch error", error);
       }
     };
@@ -58,64 +62,80 @@ function Contacts() {
   }, []);
 
   function searchContactList(event: React.ChangeEvent<HTMLInputElement>) {
-    const search = event.target.value
+    const search = event.target.value;
     setContactSearch(search);
     if (search === "") {
-      setContacts(allContacts)
+      setContacts(allContacts);
     } else {
-      const filteredContacts = allContacts.filter(contact => {
+      const filteredContacts = allContacts.filter((contact) => {
         const firstName = contact.attributes.first_name.toLowerCase();
         const lastName = contact.attributes.last_name.toLowerCase();
-        const companyName = contact.attributes.company?.name?.toLowerCase() || "";
+        const companyName =
+          contact.attributes.company?.name?.toLowerCase() || "";
 
         return (
           firstName.includes(search) ||
           lastName.includes(search) ||
           companyName.includes(search)
-          )
-      })
-      setContacts(filteredContacts)
-    }   
-  };
+        );
+      });
+      setContacts(filteredContacts);
+    }
+  }
 
   const searchBar = (
     <form onSubmit={(event) => event.preventDefault()}>
       <input
-        className='p-[1vh] border-2 border-slate-800 rounded w-[200px] h-full'
+        className="p-[1vh] border-2 border-slate-800 rounded w-[200px] h-full"
         type="search"
         id="contacts-search"
-        placeholder='Search Contacts...'
+        placeholder="Search Contacts..."
         value={contactSearch}
         onChange={searchContactList}
       />
     </form>
   );
 
-  const contactData = contacts.map(data => {
+  const contactData = contacts.map((data) => {
     const companyName = data.attributes.company?.name || "N/A";
     return (
-     <tr key={data.id} className="even:bg-gray-50 hover:bg-gray-100">
-      <td className="p-4 border-b truncate max-w-[8vw]">{data.attributes.first_name} {data.attributes.last_name}</td>
-      <td className="p-4 border-b truncate max-w-[8vw]">
-        {companyName}
-      </td>
-      <td className="p-4 border-b truncate max-w-[8vw]">{data.attributes.notes}</td>
-    </tr>
-    )
+      <tr key={data.id} className="even:bg-gray-50 hover:bg-gray-100">
+        <Link to={`/contacts/${data.id}`}>
+          <td className="p-4 border-b truncate max-w-[8vw]">
+            {data.attributes.first_name} {data.attributes.last_name}
+          </td>
+        </Link>
+        <td className="p-4 border-b truncate max-w-[8vw]">{companyName}</td>
+        <td className="p-4 border-b truncate max-w-[8vw]">
+          {data.attributes.notes}
+        </td>
+      </tr>
+    );
   });
 
   return (
-    <section className='flex'>
-      <div className='w-[70vw] pl-[4vw]'>
-        <h1 className="text-[5vh] font-bold text-cyan-600 my-[5vh]">Contacts</h1>
-        <div className='flex justify-between items-center'>
+    <section className="flex">
+      <div className="w-[70vw] pl-[4vw]">
+        <h1 className="text-[5vh] font-bold text-cyan-600 my-[5vh]">
+          Contacts
+        </h1>
+        <div className="flex justify-between items-center">
           {searchBar}
-          <Link to='/contacts/new'>
-            <button className='bg-cyan-600 text-white p-[1vh] rounded w-[10vw]'>Add New +</button>
+          <Link to="/contacts/new">
+            <button className="bg-cyan-600 text-white p-[1vh] rounded w-[10vw]">
+              Add New +
+            </button>
           </Link>
         </div>
-        {(fetchError !== null) ? <p data-cy="failed-fetch-message" className="text-center font-bold text-red-600">{fetchError}</p> : null}
-        <table className='w-[70vw] mt-[1.5vh]'>
+        {fetchError !== null ? (
+          <p
+            data-cy="failed-fetch-message"
+            className="text-center font-bold text-red-600"
+          >
+            {fetchError}
+          </p>
+        ) : null}
+        <table className="w-[70vw] mt-[1.5vh]">
           <thead className="border-t bg-gray-200">
             <tr>
               <th className="text-left p-4 border-b">Name</th>
@@ -123,14 +143,16 @@ function Contacts() {
               <th className="text-left p-4 border-b">Notes</th>
             </tr>
           </thead>
-          <tbody>
-            {contactData}
-          </tbody>
-      </table>
-        {(contacts?.length || 0) === 0 ? <p data-cy="no-contacts-message" className="text-center">No contacts saved. Click "Add New +" to start saving contacts.</p> : null}
+          <tbody>{contactData}</tbody>
+        </table>
+        {(contacts?.length || 0) === 0 ? (
+          <p data-cy="no-contacts-message" className="text-center">
+            No contacts saved. Click "Add New +" to start saving contacts.
+          </p>
+        ) : null}
       </div>
     </section>
   );
-};
+}
 
 export default Contacts;
--- src/components/contacts/ShowContact.tsx
+++ src/components/contacts/ShowContact.tsx
@@ -1,37 +1,64 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useUserLoggedContext } from '../../context/UserLoggedContext';
+
 interface ContactData {
-  id: string;
-  type: string;
-  attributes: {
-    first_name: string;
-    last_name: string;
-    company_id: number;
-    email: string;
-    phone_number: string;
-    notes: string;
-    user_id: number;
-  };
-}
+  "id": string,
+  "type": string,
+  "attributes": {
+      "first_name": string,
+      "last_name": string,
+      "company_id": number,
+      "email": string,
+      "phone_number": string,
+      "notes": string,
+      "user_id": number,
+      "company": {
+        "id": number;
+        "name": string;
+        "website": string;
+        "street_address": string;
+        "city": string;
+        "state": string;
+        "zip_code": string;
+        "notes": string;}
+  }
+};
 
 function ShowContact() {
   const { token } = useUserLoggedContext();
   const { contactId } = useParams<{ contactId: string}>();
   const contactIdInt = contactId ? Number(contactId) : null;
   const [contact, setContact] = useState<ContactData | null>(null);
+  const [fetchError, setFetchError] = useState<string | null>(null);
+
 
   // I need to create the process of fetching to the BE the #show user
   // 
   useEffect(() => {
     const fetchShowContact = async () => {
       try {
-        const token =
-          "";
-        const response = await fetch("http://localhost:3001/api/v1/users/2/contacts")
+        const response = await fetch(`http://localhost:3001/api/v1/users/2/contacts/${contactId}`, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        });
+        if (!response.ok) {
+          throw new Error(`Failed to fetch contact: ${response.statusText}`);
+        }
+        const data = await response.json();
+        setContact(data.data);
+      } catch (error) {
+        setFetchError(`${(error as Error).message}. Please try again later.`);
       }
+    };
+  
+    if (contactId) {
+      fetchShowContact();
     }
-  })
+  }, [contactId, token]);
 
   return (
   <section className="flex">
