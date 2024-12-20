143e74dc5c0f2f58c94d42c9ecdcee9e5b74c373
ROlearyPro <rolearyprogramming@gmail.com>
2024-12-20 15:14:46 -0700

    Styling changes

--- src/components/layout/MenuBar.tsx
+++ src/components/layout/MenuBar.tsx
@@ -35,7 +35,7 @@ function MenuBar() {
   // A reusable function to create NavLink classes dynamically
   const linkClasses = ({ isActive }: { isActive: boolean }) =>
     isActive
-      ? "m-auto text-cyan-800 transform scale-125 transition-transform duration-150"
+      ? "m-auto text-cyan-500 transform scale-125 transition-transform duration-150"
       : "m-auto text-white transform scale-100 transition-transform duration-150";
 
   return (
@@ -96,7 +96,7 @@ function MenuBar() {
         </nav >
                   {/* Slideout Menu */}
 
-        <nav className={"fixed overflow-hidden min-sm:invisible min-sm:hidden  z-10 bg-opacity-100 inset-0 transform ease-in-out flex flex-col justify-items-center duration-500 bg-[#046576] justify-evenly h-screen  sm:w-1/6 max-w-1/6 " + (sideMenuOpen ? " transition-opacity opacity-30 duration-500 -translate-x-full  " : " transition-all opacity-90 translate-x-0")}>
+        <nav className={"fixed overflow-hidden min-sm:invisible min-sm:hidden  z-10 bg-opacity-100 inset-0 transform ease-in-out flex flex-col justify-items-center duration-500 bg-[#046576] justify-evenly h-screen  sm:w-1/6 max-w-1/6 " + (sideMenuOpen ? " transition-opacity opacity-80 duration-500 -translate-x-full  " : " transition-all opacity-100 -translate-x-20")}>
 
           <button className="min-sm:hidden" onClick={toggleSideMenu}>
             <CloseIcon fontSize="large" data-testid="close-iconM" className="m-auto text-white justify-items-center" />
@@ -110,26 +110,31 @@ function MenuBar() {
           {/* Home */}
           <NavLink className={linkClasses} to="/home">
           <HomeIcon data-testid="home-iconM" fontSize="large" />
+          Home
           </NavLink>
 
           {/* Profile */}
           <NavLink className={linkClasses} to="/profile">
           <PersonIcon data-testid="profile-iconM" fontSize="large" />
+          Profile
           </NavLink>
 
           {/* Companies */}
           <NavLink className={linkClasses} to="/companies">
           <ApartmentIcon data-testid="companies-iconM" fontSize="large" />
+          Companies
           </NavLink>
 
           {/* Job Apps */}
           <NavLink className={linkClasses} to="/job_applications">
           <DescriptionIcon data-testid="applications-iconM" fontSize="large" />
+          Job Applications
           </NavLink>
 
           {/* Add New */}
           <NavLink className={linkClasses} to="/add-new">
           <PersonAddAlt1Icon data-testid="add-iconM" fontSize="large" />
+          Add New Contact
           </NavLink>
 
           {/* Account */}
@@ -137,16 +142,18 @@ function MenuBar() {
             to="/userInformation"
             className={({ isActive }) =>
               isActive
-                ? "m-auto  rounded-full bg-blue-500 text-white transform scale-150"
-                : "m-auto rounded-full bg-blue-200 text-white transform scale-100"
+                ? "m-auto  rounded-full text-cyan-500  text-white transform scale-150"
+                : "m-auto rounded-full  text-white transform scale-100"
             } data-testid="update-userM">
             <PersonOutlineOutlinedIcon data-testid="updateUser-iconM" fontSize="large" />
+            User Information
             </NavLink>
 
           {/* Drop Down Shortcut Menu */}
           <div className="flex mt-auto flex-col items-center justify-center relative">
-            <button className="flex items-center justify-items-center" onClick={toggleDropDown}>
+            <button className="flex items-center justify-items-center text-white" onClick={toggleDropDown}>
               <AddCircleIcon data-testid="newmenu-iconM" fontSize="large" className="m-auto text-white justify-items-center" />
+              Create New...
             </button>
 
             <ul className={`bg-cyan-600 m-4 shadow-md rounded-md transition-all duration-700 ease-in-out transform ${isDropDownOpen ? 'scale-100 opacity-100 visible' : 'scale-95 opacity-0 invisible'
@@ -165,9 +172,10 @@ function MenuBar() {
 
           {/* Account */}
           <Link to="/userInformation"
-            className="m-auto mt-[10vh] text-[2.5vw] cursor-pointer rounded-full"
+            className="m-auto mt-[10vh] text-[2.5vw] cursor-pointer text-white rounded-full"
             data-testid="update-userM">
             <PersonOutlineOutlinedIcon fontSize="large" className="m-auto w-1/5 rounded-full bg-cyan-500 text-white" />
+            Update Profile
           </Link>
 
           <div className="m-auto h-auto"></div>
