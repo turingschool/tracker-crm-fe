21bb8b30560385e5f91f3279a9315661a0d2401e
Renee Messersmith <morganrmessersmith@gmail.com>
2025-01-06 18:54:32 -0500

    Feat: updated styling on the menu bar WIP

--- src/components/layout/MenuBar.tsx
+++ src/components/layout/MenuBar.tsx
@@ -7,10 +7,13 @@ import { useUserLoggedContext } from '../../context/UserLoggedContext';
 import HomeIcon from '@mui/icons-material/Home';
 import PersonIcon from '@mui/icons-material/Person';
 import ApartmentIcon from '@mui/icons-material/Apartment';
-import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
+// import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
 import DescriptionIcon from '@mui/icons-material/Description';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
-import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+// import MenuIcon from '@mui/icons-material/Menu';
+// import CloseIcon from '@mui/icons-material/Close';
+// import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 function MenuBar() {
   const [isDropDownOpen, setIsDropDownOpen] = useState(false);
@@ -23,18 +26,23 @@ function MenuBar() {
   console.log(userId);
 
   // A reusable function to create NavLink classes dynamically
+  // const linkClasses = ({ isActive }: { isActive: boolean }) =>
+  //   isActive
+  //     ? "m-auto text-cyan-800 transform scale-150 transition-transform duration-150"
+  //     : "m-auto text-gray-500 transform scale-125 transition-transform duration-150";
+
   const linkClasses = ({ isActive }: { isActive: boolean }) =>
     isActive
-      ? "m-auto text-cyan-800 transform scale-125 transition-transform duration-150"
-      : "m-auto text-white transform scale-100 transition-transform duration-150";
+      ? "m-auto mt-[3vh] text-cyan-800 transform scale-150 transition-transform duration-150"
+      : "m-auto mt-[3vh] text-gray-500 transform scale-125 transition-transform duration-150";
 
   return (
     <div className="flex flex-row h-screen">
-      <nav className="flex flex-col justify-items-center bg-gray-500 justify-evenly h-full mr-10 md:w-1/6 max-w-1/6">
+      <nav className="flex flex-col justify-items-center justify-evenly h-full w-28">
         
         {/* Logo */}
         <Link className="m-auto cursor-pointer" to="/">
-          <img className="m-auto w-1/2 cursor-pointer" src={logo} alt="Logo" />
+          <img className="m-auto cursor-pointer" src={logo} alt="Logo" />
         </Link>
 
         {/* Home */}
@@ -43,42 +51,24 @@ function MenuBar() {
         </NavLink>
 
         {/* Contacts */}
-        <Link className="m-auto cursor-pointer" to="/contacts">
-          <PersonIcon fontSize="large" className="m-auto text-white" />
-        </Link>
+        <NavLink className={linkClasses} to="/contacts">
+          <PersonIcon fontSize="large" />
+        </NavLink>
 
         {/* Companies */}
-        <Link className="m-auto cursor-pointer" to="/companies">
-          <ApartmentIcon fontSize="large" className="m-auto text-white" />
-        </Link>
+        <NavLink className={linkClasses} to="/companies">
+          <ApartmentIcon fontSize="large" />
+        </NavLink>
 
         {/* Job Apps */}
         <NavLink className={linkClasses} to="/job_applications">
           <DescriptionIcon fontSize="large" />
         </NavLink>
 
-        {/* Add New */}
-        <NavLink className={linkClasses} to="/add-new">
-          <PersonAddAlt1Icon fontSize="large" />
-        </NavLink>
-
-        {/* Account */}
-        <NavLink
-          to="/userInformation"
-          className={({ isActive }) =>
-            isActive 
-          ? "m-auto  rounded-full bg-blue-500 text-white transform scale-150" 
-          : "m-auto rounded-full bg-blue-200 text-white transform scale-100"
-          }
-          data-testid="update-user"
-        >
-          <PersonOutlineOutlinedIcon fontSize="large" />
-        </NavLink>
-
       {/* Drop Down Shortcut Menu */}
       <div className="flex flex-col items-center justify-center relative">
         <button className="flex items-center justify-items-center" onClick={toggleDropDown}>
-          <AddCircleIcon fontSize="large" className="m-auto text-white justify-items-center" />
+          <AddCircleIcon fontSize="large" className="m-auto text-gray-500 justify-items-center mt-7" />
         </button>
         
         <ul
@@ -97,12 +87,34 @@ function MenuBar() {
           </li>
         </ul>
       </div>
+
+        {/* Account */}
+        <NavLink
+          to="/userInformation"
+          className={({ isActive }) =>
+            isActive 
+          ? "m-auto  rounded-full bg-cyan-800 text-white " 
+          : "m-auto rounded-full text-gray-500 transform scale-100"
+          }
+          data-testid="update-user"
+        >
+          <AccountCircleIcon fontSize="large" />
+        </NavLink>
+
       </nav>
+      <div className='quad-color-bar flex flex-col w-2 h-[100%]'>
+        <div className='cyan-bar bg-cyan-500 w-[100%] h-[25%]'/>
+        <div className='yellow-bar bg-yellow-500 w-[100%] h-[25%]'/>
+        <div className='red-bar bg-red-500 w-[100%] h-[25%]'/>
+        <div className='green-bar bg-green-500 w-[100%] h-[25%]'/>
+      </div>
       
       {/* This is where nested routes will render. */}
       <div className="flex-grow p-4">
         <Outlet />
       </div>
+
+
     </div>
   );
 }
