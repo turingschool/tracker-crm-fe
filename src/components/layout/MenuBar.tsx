b8fe68a6d0102958147a3e81d47463cbaf498c2e
Renee Messersmith <morganrmessersmith@gmail.com>
2025-01-07 17:09:15 -0500

    Feat: added colored bar to nav bar and changed colors to match comp photo

--- src/components/layout/MenuBar.tsx
+++ src/components/layout/MenuBar.tsx
@@ -14,8 +14,9 @@ import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
 // import plusOpen from '../../components/icons/plus-open.png';
 // import plus from '../../components/icons/plus-small_4338829.png'; 
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
-import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+// import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 
 function MenuBar() {
   const [sideMenuOpen, setSideMenuOpen] = useState(true)
@@ -35,26 +36,26 @@ function MenuBar() {
   // A reusable function to create NavLink classes dynamically
   const linkClasses = ({ isActive }: { isActive: boolean }) =>
     isActive
-      ? "m-auto text-cyan-500 transform scale-125 transition-transform duration-150"
-      : "m-auto text-white transform scale-100 transition-transform duration-150";
+      ? "m-auto text-cyan-800 transform scale-150 transition-transform duration-150"
+      : "m-auto text-gray-500 transform scale-125 transition-transform duration-150";
 
   return (
-    <div className='flex flex-row'>
+    <div className='flex h-screen'>
       {/* // Menu */}
-      <nav className='flex flex-col   bg-[#046576] justify-evenly h-screen  sm:w-1/12  ' >
+      <nav className='flex flex-col justify-items-center justify-evenly h-dvh w-28'>
 
         {/* Open button for Slideout Menu */}
-        <button className="flex items-center justify-items-center" onClick={toggleSideMenu}>
-          <MenuIcon data-testid="menu-iconM" fontSize="large" className="m-auto text-white justify-items-center" />
+        <button className="flex items-center justify-items-center visible sm:invisible" onClick={toggleSideMenu}>
+          <MenuIcon data-testid="menu-iconM" fontSize="large" className="m-auto justify-items-center" />
         </button>
 
 
         {/* Desktop Menu */}
-        < nav className='flex flex-col fixed bg-[#046576] max-sm:hidden  justify-items-center h-screen sm:w-1/12 ' >
+        < nav className='flex flex-col fixed max-sm:hidden justify-items-center h-screen sm:w-1/12 ' >
 
           {/* Logo */}
           <Link className="m-auto cursor-pointer" to="/">
-            <img className="m-auto w-1/2 cursor-pointer" src={logo} alt="Logo" />
+            <img className="m-auto cursor-pointer" src={logo} alt="Logo" />
           </Link>
 
           {/* Home */}
@@ -78,9 +79,32 @@ function MenuBar() {
           </NavLink>
 
           {/* Add New */}
-          <NavLink className={linkClasses} to="/add-new">
+          {/* <NavLink className={linkClasses} to="/add-new">
             <PersonAddAlt1Icon data-testid="add-iconD" fontSize="large" />
-          </NavLink>
+          </NavLink> */}
+
+           {/* Drop Down Shortcut Menu */}
+          <div className="flex flex-col items-center justify-center relative">
+            <button className="flex items-center justify-items-center" onClick={toggleDropDown}>
+              <AddCircleIcon fontSize="large" className="m-auto text-gray-500 justify-items-center mt-7" />
+            </button>
+            
+            <ul
+              className={`bg-cyan-600 m-4 shadow-md rounded-md transition-all duration-700 ease-in-out transform ${
+                isDropDownOpen ? 'scale-100 opacity-100 visible' : 'scale-95 opacity-0 invisible'
+              }`}
+            >
+              <li className="p-2 hover:bg-gray-100 rounded text-center mb-2 font-[Helvetica Neue]">
+              <Link to="/contacts/new">Create New Contact</Link>
+              </li>
+              <li className="p-2 hover:bg-gray-100 rounded text-center mb-2 font-[Helvetica Neue]">
+                <Link to="/companies/new">Create New Company</Link>
+              </li>
+              <li className="p-2 hover:bg-gray-100 rounded text-center font-[Helvetica Neue]">
+                <Link to="/jobapplications/new">Create New Job Application</Link>
+              </li>
+            </ul>
+          </div>
 
           {/* Account */}
           <NavLink
@@ -90,12 +114,12 @@ function MenuBar() {
                 ? "m-auto  rounded-full bg-blue-500 text-white transform scale-150"
                 : "m-auto rounded-full bg-blue-200 text-white transform scale-100"
             } data-testid="update-user">
-            <PersonOutlineOutlinedIcon data-testid="updateUser-iconD" fontSize="large" />
+            <AccountCircleIcon data-testid="updateUser-iconD" fontSize="large" />
           </NavLink>
           <div className="m-auto h-auto"></div>
         </nav >
-                  {/* Slideout Menu */}
 
+        {/* Slideout Menu */}
         <nav className={"fixed overflow-hidden min-sm:invisible min-sm:hidden  z-10 bg-opacity-100 inset-0 transform ease-in-out flex flex-col justify-items-center duration-500 bg-[#046576] justify-evenly h-screen  sm:w-1/6 max-w-1/6 " + (sideMenuOpen ? " transition-opacity opacity-80 duration-500 -translate-x-full  " : " transition-all opacity-100 -translate-x-20")}>
 
           <button className="min-sm:hidden" onClick={toggleSideMenu}>
@@ -145,7 +169,7 @@ function MenuBar() {
                 ? "m-auto  rounded-full text-cyan-500  text-white transform scale-150"
                 : "m-auto rounded-full  text-white transform scale-100"
             } data-testid="update-userM">
-            <PersonOutlineOutlinedIcon data-testid="updateUser-iconM" fontSize="large" />
+            <AccountCircleIcon data-testid="updateUser-iconM" fontSize="large" />
             User Information
             </NavLink>
 
@@ -174,7 +198,7 @@ function MenuBar() {
           <Link to="/userInformation"
             className="m-auto mt-[10vh] text-[2.5vw] cursor-pointer text-white rounded-full"
             data-testid="update-userM">
-            <PersonOutlineOutlinedIcon fontSize="large" className="m-auto w-1/5 rounded-full bg-cyan-500 text-white" />
+            <AccountCircleIcon fontSize="large" className="m-auto w-1/5 rounded-full bg-cyan-500 text-white" />
             Update Profile
           </Link>
 
@@ -182,7 +206,17 @@ function MenuBar() {
         </nav >
 
       </nav>
-      <div className='flex-1 m-auto'>
+      <div className='quad-color-bar flex flex-col w-[1%] h-[100%]'>
+        <div className='cyan-bar bg-cyan-500 w-[100%] h-[25%]'/>
+        <div className='yellow-bar bg-yellow-500 w-[100%] h-[25%]'/>
+        <div className='red-bar bg-red-500 w-[100%] h-[25%]'/>
+        <div className='green-bar bg-green-500 w-[100%] h-[25%]'/>
+      </div>
+
+
+
+      {/* <div className='flex-1 m-auto'> */}
+      <div className='flex-grow p-4'>
         <Outlet />
       </div>
     </div>
