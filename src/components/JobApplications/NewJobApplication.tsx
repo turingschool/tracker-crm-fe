34341c4987f33c6634b70d227250e404dc89f606
Joe Chirchirillo <cchirchirillo3@gmail.com>
2025-01-06 23:16:49 -0700

    feat: add new status options

--- src/components/JobApplications/NewJobApplication.tsx
+++ src/components/JobApplications/NewJobApplication.tsx
@@ -47,6 +47,8 @@ function NewJobApplication() {
     3: 'Offer',
     4: 'Rejected',
     5: 'Phone Screen',
+    6: 'Code Challenge',
+    7: 'Not Yet Applied',
   };
 
   const statusStyles: { [key: string]: string } = {
@@ -55,6 +57,8 @@ function NewJobApplication() {
     Offer: 'bg-teal-300 text-teal-900',
     Rejected: 'bg-red-200 text-red-800',
     'Phone Screen': 'bg-yellow-300 text-yellow-900',
+    'Code Challenge': 'bg-blue-200 text-blue-800',
+    'Not Yet Applied': 'bg-orange-200 text-orange-800',
   };
 
   useEffect(() => {
