7b687947322b931d8908b692c7b078ab2ec4c219
Marshall Hotaling <marshall.hotaling@gmail.com>
2025-01-08 13:22:42 -0700

    miss commit

--- src/components/dashboard/DashBoard.tsx
+++ src/components/dashboard/DashBoard.tsx
@@ -1,13 +1,35 @@
 import {useUserLoggedContext} from "../../context/UserLoggedContext";
+import { fetchDashBoardData } from '../../apiCalls';
 import React from 'react'
 export const DashBoard : React.FC = () => {
-    const {isLoggedIn, clearUserLogged, userData,token } = useUserLoggedContext()
+
+    const {isLoggedIn, userData,token } = useUserLoggedContext()
     if(token && isLoggedIn){
         return (
             <>
-                <label className="bg-pink-500">Hello world</label>
-                <h1>Welcome, {userData.user.data.attributes.name}</h1>
-                <button onClick={()=>clearUserLogged}>Log Out</button>
+                <div>
+                    <h1>Welcome, {userData.user.data.attributes.name}</h1>
+                </div>
+
+                <div className="bg-pink-500">
+                    <label>Jobs</label>
+                    <label>{dashBoardData.data.id}</label>
+                    <label></label>
+                </div>
+
+                <div className="bg-blue-700">
+                    <label>Contacts</label>
+                    <label></label>
+                    <label></label>
+                </div>
+
+                <div className='bg-green-500'>
+                    <label>Companies</label>
+                    <label></label>
+                    <label></label>
+                </div>
+
+
             </>
         )
     } else {
