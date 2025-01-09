fba3c55894f31c826c526ebce88a2f55270339a4
Marshall Hotaling <marshall.hotaling@gmail.com>
2025-01-09 11:37:44 -0700

    compleated cypress happy path
    intorcepted all fetch calls and replaced with moc data needs to have sad path implumented

--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -37,25 +37,25 @@ export const DashBoard : React.FC = () => {
         return (
             <>
                 <div className="fixed top-[15vh] left-[25vh] right-0 font-bold font-size-[4vh] text-5xl text-cyan-600">
-                    <h1>Welcome, {userData.user.data.attributes.name}</h1>
+                    <h1>Welcome,{userData.user.data.attributes.name}</h1>
                 </div>
                 <div className="ml-2 fixed top-[20vh] left-[25vh]">
 
                     <div className="bg-white w-[20vw] h-[17vh] inline-block rounded-[20px] shadow-black shadow-xl">
                         <label className='text-center block text-[20px] font-bold text-cyan-600 mt-1'>Jobs</label>
-                        <label className='text-center block mt-1 font-bold text-[50px] text-cyan-600'>{`${dashData.job_applications.length }`}</label>
+                        <label className='text-center block mt-1 font-bold text-[50px] text-cyan-600' data-cy="dashJobNum">{`${dashData.job_applications.length }`}</label>
                         <label className='text-center block text-[20px] font-bold text-cyan-600 mt-1'>Apps submitted this week</label>
                     </div>
 
                     <div className="bg-white w-[20vw] h-[17vh] inline-block m-24 rounded-[20px] shadow-black shadow-xl">
                         <label className='text-center block text-[20px] font-bold text-cyan-600 mt-2'>Contacts</label>
-                        <label className='text-center block mt-1 font-bold text-[50px] text-cyan-600'>{`${dashData.contacts.length}`}</label>
-                        <label className='text-center block text-[20px] font-bold text-cyan-600 mt-1'>New connections this week</label>
+                        <label className='text-center block mt-1 font-bold text-[50px] text-cyan-600' data-cy="dashConNum">{`${dashData.contacts.length}`}</label>
+                        <label className='text-center block text-[20px] font-bold text-cyan-600 mt-1' data-cy="conLabel">New connections this week</label>
                     </div>
 
                     <div className='bg-white w-[20vw] h-[17vh] inline-block rounded-[20px] shadow-black shadow-xl'>
                         <label className='text-center block text-[20px] font-bold text-cyan-600 mt-2'>Companies</label>
-                        <label className='text-center block mt-1 font-bold text-[50px] text-cyan-600'>{`${dashData.companies.length}`}</label>
+                        <label className='text-center block mt-1 font-bold text-[50px] text-cyan-600' data-cy="dashCompNum">{`${dashData.companies.length}`}</label>
                         <Link className='text-center block text-[20px] font-bold bg-cyan-600 mt-1 w-[16vw] text-white ml-[3vh]' to="/companies/new">Add new company</Link>
 
                     </div>
