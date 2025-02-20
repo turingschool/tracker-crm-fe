46460063cee29884cac613ae44e8ebd432031a98
Jeremiah Ross <crosswolf.20work@gmail.com>
2025-02-20 16:28:11 -0600

    Feat/Imported logout functions from the useuserLoggedContext component

--- src/components/layout/MenuBar.tsx
+++ src/components/layout/MenuBar.tsx
@@ -14,6 +14,7 @@ import CloseIcon from "@mui/icons-material/Close";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 function MenuBar() {
+  const { clearUserLogged } = useUserLoggedContext();
   const [sideMenuOpen, setSideMenuOpen] = useState(true);
   const toggleSideMenu = () => {
     setSideMenuOpen((previousState) => !previousState);
@@ -23,6 +24,11 @@ function MenuBar() {
   const { userData } = useUserLoggedContext();
   const toggleDropDown = () => {
     setIsDropDownOpen((prevState) => !prevState);
+  };
+
+  const [isUserDropDownOpen, setIsUserDropDownOpen] = useState(false);
+  const toggleUserDropDown = () => {
+    setIsUserDropDownOpen((prevState) => !prevState);
   };
 
   let { userId = userData.user.data.id } = useParams();
