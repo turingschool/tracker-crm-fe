024ce1391b8aa6ed4e72dde635d00530ad6116a3
Michael O'Brien <38501347+MiTOBrien@users.noreply.github.com>
2025-02-27 07:00:14 -0500

    Update dashBoardLinksSpec.cy.js
    Uncommented out needed test code.

--- cypress/e2e/dashBoardLinksSpec.cy.js
+++ cypress/e2e/dashBoardLinksSpec.cy.js
@@ -35,81 +35,81 @@ describe('Dash Board after loggging in with recent jobs, contacts and companies'
     cy.get('[data-testid="login-button"]').click();
   })
 
-  // it("Should have a header with the users name once data is loaded", () => {
-  //   cy.get("h1").should("have.text", "Welcome, Danny DeVito");
-  // });
+  it("Should have a header with the users name once data is loaded", () => {
+    cy.get("h1").should("have.text", "Welcome, Danny DeVito");
+  });
 
-  // it("Should be clickable to route you to job applications", () => {
-  //   cy.intercept(
-  //     "GET",
-  //     "http://localhost:3001/api/v1/users/1/job_applications",
-  //     (req) => {
-  //       req.on("response", (res) => {
-  //         res.setDelay(2000);
-  //       });
-  //       req.reply({
-  //         statusCode: 200,
-  //         fixture: "mockJobApps",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //         },
-  //       });
-  //     }
-  //   ).as("getJobApplications");
+  it("Should be clickable to route you to job applications", () => {
+    cy.intercept(
+      "GET",
+      "http://localhost:3001/api/v1/users/1/job_applications",
+      (req) => {
+        req.on("response", (res) => {
+          res.setDelay(2000);
+        });
+        req.reply({
+          statusCode: 200,
+          fixture: "mockJobApps",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+      }
+    ).as("getJobApplications");
 
-  //   cy.get("label").contains('Jobs').click();
-  //   cy.url().should('include', 'http://localhost:3000/job_applications');
-  // });
+    cy.get("label").contains('Jobs').click();
+    cy.url().should('include', 'http://localhost:3000/job_applications');
+  });
 
-  // it("Should be clickable to route you to contacts", () => {
-  //   cy.intercept(
-  //     "GET",
-  //     "http://localhost:3001/api/v1/users/2/companies/1/contacts",
-  //     {
-  //       statusCode: 200,
-  //       body: {
-  //         contacts: {
-  //           data: [
-  //             {
-  //               id: "2",
-  //               type: "contact",
-  //               attributes: {
-  //                 first_name: "Jane",
-  //                 last_name: "Smith",
-  //                 email: "jane@example.com",
-  //                 phone_number: "987-654-3210",
-  //                 notes: "Another team member at Future Designs LLC",
-  //               },
-  //             },
-  //           ],
-  //         },
-  //       },
-  //     }
-  //   ).as("get-company-contacts");
+  it("Should be clickable to route you to contacts", () => {
+    cy.intercept(
+      "GET",
+      "http://localhost:3001/api/v1/users/2/companies/1/contacts",
+      {
+        statusCode: 200,
+        body: {
+          contacts: {
+            data: [
+              {
+                id: "2",
+                type: "contact",
+                attributes: {
+                  first_name: "Jane",
+                  last_name: "Smith",
+                  email: "jane@example.com",
+                  phone_number: "987-654-3210",
+                  notes: "Another team member at Future Designs LLC",
+                },
+              },
+            ],
+          },
+        },
+      }
+    ).as("get-company-contacts");
 
-  //   cy.get("label").contains('Contacts').click();
-  //   cy.url().should('include', 'http://localhost:3000/contacts');
-  // });
+    cy.get("label").contains('Contacts').click();
+    cy.url().should('include', 'http://localhost:3000/contacts');
+  });
 
-  // it("Should be clickable to route you to companies", () => {
-  //   cy.intercept("GET", `http://localhost:3001/api/v1/users/1/companies`, {
-  //     statusCode: 200,
-  //     body: mockCompanies,
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       Authorization: "Bearer fake-token",
-  //     },
-  //   }).as("getCompanies");
+  it("Should be clickable to route you to companies", () => {
+    cy.intercept("GET", `http://localhost:3001/api/v1/users/1/companies`, {
+      statusCode: 200,
+      body: mockCompanies,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer fake-token",
+      },
+    }).as("getCompanies");
 
-  //   cy.get("label").contains('Companies').click();
-  //   cy.url().should('include', 'http://localhost:3000/companies');
-  // });
+    cy.get("label").contains('Companies').click();
+    cy.url().should('include', 'http://localhost:3000/companies');
+  });
 
-  // it("Should not show the button with at least 1 job application/contact/company", () => {
-  //   cy.get('[data-cy="jobApplicationBtn"]').should('not.exist');
-  //   cy.get('[data-cy="contactBtn"]').should('not.exist');
-  //   cy.get('[data-cy="companyBtn"]').should('not.exist');
-  // })
+  it("Should not show the button with at least 1 job application/contact/company", () => {
+    cy.get('[data-cy="jobApplicationBtn"]').should('not.exist');
+    cy.get('[data-cy="contactBtn"]').should('not.exist');
+    cy.get('[data-cy="companyBtn"]').should('not.exist');
+  })
 
   it("Should display button with no job applications and go to the new job application page when the button is clicked", () => {
     cy.intercept("POST", "http://localhost:3001/api/v1/sessions", {
