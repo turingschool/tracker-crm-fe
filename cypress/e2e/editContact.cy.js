da0fa0c89a886745f030c4414b496ef0ddd51e3a
Kristin Weiland <kristinweiland@gmail.com>
2025-04-08 22:14:47 -0700

    Update edit contact test to use aria-label selector for modal close button

--- cypress/e2e/editContact.cy.js
+++ cypress/e2e/editContact.cy.js
@@ -128,7 +128,7 @@ describe("Edit Contact Functionality", () => {
 
   it("Should close the modal when clicking the X button", () => {
     cy.get("button").contains("Edit").click();
-    cy.get(".absolute.top-2.right-2").click({ force: true });
+    cy.get('[aria-label="Close modal"]').click();
     cy.get(".bg-white").should("not.exist");
   });
 
