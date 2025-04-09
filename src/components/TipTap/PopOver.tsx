6204169d51149a748271c28277083666fd96c574
elysableu <elysabward@gmail.com>
2025-04-09 13:32:07 -0600

    Refactor popover component to use @mui icons instead of react icons

--- src/components/TipTap/PopOver.tsx
+++ src/components/TipTap/PopOver.tsx
@@ -1,6 +1,9 @@
-import { FaBold, FaItalic, FaUnderline, FaEdit } from 'react-icons/fa';
 import { BubbleMenu, BubbleMenuProps } from '@tiptap/react';
 import { PopOverProps } from '../../Interfaces';
+import FormatBold from '@mui/icons-material/FormatBold';
+import FormatItalic from '@mui/icons-material/FormatItalic';
+import FormatUnderlined from '@mui/icons-material/FormatUnderlined';
+import Highlight from '@mui/icons-material/Highlight';
 
 const PopOver: React.FC<PopOverProps> = ({ editor, onClose }) => {
   if (!editor) return null;
@@ -44,35 +47,35 @@ const PopOver: React.FC<PopOverProps> = ({ editor, onClose }) => {
           e.preventDefault();
           e.stopPropagation();
         }}
-        className={'pointer-events-none z-10000 flex w-[20em] p-0 bg-gray-200 rounded-lg justify-around items-center divide-x divide-gray-400'}
+        className={'pointer-events-none z-10000 flex w-[24em] p-0 bg-gray-200 rounded-lg justify-around items-center divide-x divide-gray-400'}
       >
         <button 
           onMouseDown={(e) => e.stopPropagation()}
           onClick={handleBold} 
           className={`${editor.isActive('bold') ? 'bg-gray-200 !important' : 'bg-transparent'} p-3 m-0 flex-1 pointer-events-auto flex justify-center items-center rounded-l-lg`}
         >
-          <FaBold />
+          <FormatBold />
         </button>
         <button 
           onMouseDown={(e) => e.stopPropagation()}
           onClick={handleItalic} 
           className={`${editor.isActive('italic') ? 'bg-gray-200' : 'bg-transparent'} p-3 m-0 flex-1 pointer-events-auto flex justify-center items-center`}
         >
-          <FaItalic />
+          <FormatItalic />
         </button>
         <button 
           onMouseDown={(e) => e.stopPropagation()}
           onClick={handleUnderline} 
           className={`${editor.isActive('underline') ? 'bg-gray-200' : 'bg-transparent'} p-3 m-0 flex-1 pointer-events-auto flex justify-center items-center`}
         >
-          <FaUnderline />
+          <FormatUnderlined />
         </button>
         <button 
           onMouseDown={(e) => e.stopPropagation()}
           onClick={handleHighlight} 
           className={`${editor.isActive('highlight') ? 'bg-gray-200' : 'bg-transparent'} p-3 m-0 flex-1 pointer-events-auto flex justify-center items-center rounded-r-lg`}
         >
-          <FaEdit />
+          <Highlight />
         </button>
       </div>
     </BubbleMenu>
