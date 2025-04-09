c8de9b8a5b8bebc1e777010ac0a48e573b8317e2
elysableu <elysabward@gmail.com>
2025-04-09 13:59:40 -0600

    Fix bug where clicking inside the editor triggers first formatting

--- src/components/TipTap/PopOver.tsx
+++ src/components/TipTap/PopOver.tsx
@@ -8,36 +8,35 @@ import Highlight from '@mui/icons-material/Highlight';
 const PopOver: React.FC<PopOverProps> = ({ editor, onClose }) => {
   if (!editor) return null;
   
-  const shouldShow: BubbleMenuProps['shouldShow']= ({ editor }) => {
-    const { empty } = editor.state.selection;
-    return !empty; 
+  const shouldShow: BubbleMenuProps['shouldShow']= ({ editor, from, to }) => {
+    return from !== to && editor.isEditable;
   }
 
   const handleBold = (e: React.MouseEvent) => {
     e.preventDefault();
     
-    editor.chain().toggleBold().run(); 
+    editor.chain().focus().toggleBold().run(); 
     onClose?.();
   }
 
   const handleItalic = (e: React.MouseEvent) => {
     e.preventDefault();
     
-    editor.chain().toggleItalic().run(); 
+    editor.chain().focus().toggleItalic().run(); 
     onClose?.();
   }
 
   const handleUnderline = (e: React.MouseEvent) => {
     e.preventDefault();
     
-    editor.chain().toggleUnderline().run(); 
+    editor.chain().focus().toggleUnderline().run(); 
     onClose?.();
   }
 
   const handleHighlight = (e: React.MouseEvent) => {
     e.preventDefault();
     
-    editor.chain().toggleHighlight().run();
+    editor.chain().focus().toggleHighlight().run();
     onClose?.();
   }
 
@@ -47,33 +46,45 @@ const PopOver: React.FC<PopOverProps> = ({ editor, onClose }) => {
           e.preventDefault();
           e.stopPropagation();
         }}
-        className={'pointer-events-none z-10000 flex w-[24em] p-0 bg-gray-200 rounded-lg justify-around items-center divide-x divide-gray-400'}
+        className={'pointer-events-none z-10000 flex w-[24em] p-0 bg-gray-200 rounded-lg justify-around items-center divide-x divide-gray-500'}
       >
         <button 
-          onMouseDown={(e) => e.stopPropagation()}
+          onMouseDown={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+          }}          
           onClick={handleBold} 
-          className={`${editor.isActive('bold') ? 'bg-gray-200 !important' : 'bg-transparent'} p-3 m-0 flex-1 pointer-events-auto flex justify-center items-center rounded-l-lg`}
+          className={`${editor.isActive('bold') ? 'bg-gray-400' : 'bg-transparent'} p-3 m-0 flex-1 pointer-events-auto flex justify-center items-center rounded-l-lg`}
         >
           <FormatBold />
         </button>
         <button 
-          onMouseDown={(e) => e.stopPropagation()}
+          onMouseDown={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+          }}          
           onClick={handleItalic} 
-          className={`${editor.isActive('italic') ? 'bg-gray-200' : 'bg-transparent'} p-3 m-0 flex-1 pointer-events-auto flex justify-center items-center`}
+          className={`${editor.isActive('italic') ? 'bg-gray-400' : 'bg-transparent'} p-3 m-0 flex-1 pointer-events-auto flex justify-center items-center`}
         >
           <FormatItalic />
         </button>
         <button 
-          onMouseDown={(e) => e.stopPropagation()}
+          onMouseDown={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+          }}          
           onClick={handleUnderline} 
-          className={`${editor.isActive('underline') ? 'bg-gray-200' : 'bg-transparent'} p-3 m-0 flex-1 pointer-events-auto flex justify-center items-center`}
+          className={`${editor.isActive('underline') ? 'bg-gray-400' : 'bg-transparent'} p-3 m-0 flex-1 pointer-events-auto flex justify-center items-center`}
         >
           <FormatUnderlined />
         </button>
         <button 
-          onMouseDown={(e) => e.stopPropagation()}
+          onMouseDown={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+          }}          
           onClick={handleHighlight} 
-          className={`${editor.isActive('highlight') ? 'bg-gray-200' : 'bg-transparent'} p-3 m-0 flex-1 pointer-events-auto flex justify-center items-center rounded-r-lg`}
+          className={`${editor.isActive('highlight') ? 'bg-gray-400' : 'bg-transparent'} p-3 m-0 flex-1 pointer-events-auto flex justify-center items-center rounded-r-lg`}
         >
           <Highlight />
         </button>
--- src/components/TipTap/TipTap.tsx
+++ src/components/TipTap/TipTap.tsx
@@ -1,5 +1,5 @@
 // src/Tiptap.tsx
-// import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { EditorContent, useEditor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import { TipTapProps } from '../../Interfaces';
@@ -9,6 +9,7 @@ import Highlight from '@tiptap/extension-highlight';
 import PopOver from './PopOver';
 
 const TipTap: React.FC<TipTapProps> = ({ content, onChange}) => {
+  // Configure editor
   const editor = useEditor({
     extensions: [
       StarterKit,
@@ -33,6 +34,34 @@ const TipTap: React.FC<TipTapProps> = ({ content, onChange}) => {
       },
     },
   })
+
+  // Close popover and unselect text when clicking in editor outside of popover
+  useEffect(() => {
+    if (!editor) return;
+
+    const editorEl = editor.view.dom;
+
+    const handleMouseDown = (e: MouseEvent) => {
+      const { state } = editor;
+      const { from, to } = state.selection;
+
+      if (from !== to) {
+        const start = editor.view.domAtPos(from).node;
+        const end = editor.view.domAtPos(to).node;
+
+        if (start.contains(e.target as Node) || end.contains(e.target as Node)) {
+          editor.commands.blur();
+          editor.commands.setTextSelection({from: to, to });
+        }
+      }
+    };
+
+    editorEl.addEventListener('mousedown', handleMouseDown);
+
+    return () => {
+      editorEl.removeEventListener('mousedown', handleMouseDown);
+    };
+  }, [editor]);
 
   if (!editor) return null;
   
