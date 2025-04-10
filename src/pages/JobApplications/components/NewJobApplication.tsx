e8f4306d83b44907e8b6b04af2656110dff61beb
elysableu <elysabward@gmail.com>
2025-04-10 15:01:03 -0600

    Update imports in newJobApplocation component

--- src/pages/JobApplications/components/NewJobApplication.tsx
+++ src/pages/JobApplications/components/NewJobApplication.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useUserLoggedContext } from '../../context/UserLoggedContext';
-import { statusMap, statusStyles} from "../JobApplicationUtilities";
-import { fetchContacts, fetchCompanies } from "../../apiCalls";
-import { postJobApplication } from '../../trackerApiCalls';
-import TipTap from '../TipTap/TipTap';
+import { useUserLoggedContext } from '../../../context/UserLoggedContext';
+import { statusMap, statusStyles} from "../../../pages/JobApplications/components/JobApplicationUtilities";
+import { fetchContacts, fetchCompanies } from "../../../apiCalls";
+import { postJobApplication } from '../../../trackerApiCalls';
+import TipTap from '../../../components/TipTap/TipTap';
 
 interface Company {
   id: string;
