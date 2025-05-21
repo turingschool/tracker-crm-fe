d834f39ea5d3bc8c92443f6320c6ebb51d8c735b
Mark Kendall <mkendall.tutor@gmail.com>
2025-05-21 01:30:53 -0500

    Update configuration parameters in index.js, fix typo

--- src/index.js
+++ src/index.js
@@ -6,20 +6,20 @@ import { BrowserRouter } from 'react-router-dom';
 import { UserLoggedContextProvider } from './context/UserLoggedContext';
 
 import { HoneycombWebSDK } from '@honeycombio/opentelemetry-web'
-import { getWebAutoInstrumentations } from '@opentelemtry/auto-instrumentations-web'
+import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web'
 
 const configDefaults = {
-  ignoreNetworkEvents: true,
+  ignoreNetworkEvents: true
   // propagateTraceHeaderCorsUrls: [
   // /.+/g
-  //This looks like regex.  Specify the URL of the domain we want to include (I assume just the BE?)
+  //This looks like regex.  Specify the URL of the domain we want to include (I assume just the BE?).  It looks like CORS headers might get intercepted or something otherwise?
   // ]
 }
 
 const sdk = new HoneycombWebSDK({
   // endpoint: "https://api.eu1.honeycomb.io/v1/traces", // Send to EU instance of Honeycomb. Defaults to sending to US instance.
   debug: true, // Set to false for production environment.  NOTE: isn't the Heroku version 'production' in essence?
-  apiKey: `${import.meta.env.HONEYCOMB_API_KEY}`, // Replace with your Honeycomb Ingest API Key.  NOTE: I hope this is the same as the main API key I extracted earlier...
+  apiKey: `${process.env.HONEYCOMB_API_KEY}`, //NOTE: this is supposed to be the "Honeycomb Ingest API key".  I hope this is the same as the main API key I extracted earlier...
   serviceName: 'tracker-crm-fe', // Replace with your application name. Honeycomb uses this string to find your dataset when we receive your data. When no matching dataset exists, we create a new one with this name if your API Key has the appropriate permissions.
   instrumentations: [getWebAutoInstrumentations({
     // Loads custom configuration for xml-http-request instrumentation.
