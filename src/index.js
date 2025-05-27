a6e8e2cf3bb48f50be3c2e94550ede3a471d5471
Mark Kendall <mkendall.tutor@gmail.com>
2025-05-27 13:38:32 -0500

    Fix: update relevant .env files and formatting for proper env variable usage

--- src/index.js
+++ src/index.js
@@ -16,9 +16,20 @@ const configDefaults = {
   // ]
 }
 
+console.log(process.env.VITE_RANDOM_VAR)
+// console.log(import.meta.env.VITE_RANDOM_VAR)
+
+const honeycombApiKey = process.env.REACT_APP_HONEYCOMB_API_INGEST_KEY
+
 const sdk = new HoneycombWebSDK({
   debug: false, // Set to false for production environment.  NOTE: isn't the Heroku version 'production' in essence?
-  apiKey: `${process.env.HONEYCOMB_API_INGEST_KEY}`, //NOTE: this is supposed to be the "Honeycomb Ingest API key".  I hope this is the same as the main API key I extracted earlier...
+  //NOTE: if I type the plaintext API key, it works.  WHY!?!?!  Something is clearly not working correctly with the env vars.
+  // apiKey: 'hcaik_01jvrs56j54sf3ykggjz1dehsmk108bnj2qqf4qhtntmdtw0snv6hgjgge',
+  // apiKey: process.env.HONEYCOMB_API_CONFIG_KEY, //NOTE: this is supposed to be the "Honeycomb Ingest API key".  I hope this is the same as the main API key I extracted earlier...
+  // apiKey: `${process.env.HONEYCOMB_API_INGEST_KEY}`, //NOTE: this is supposed to be the "Honeycomb Ingest API key".  I hope this is the same as the main API key I extracted earlier...
+  apiKey: honeycombApiKey,
+  // apiKey: `${process.env.HONEYCOMB_API_INGEST_KEY}`, //NOTE: this is supposed to be the "Honeycomb Ingest API key".  I hope this is the same as the main API key I extracted earlier...
+  // apiKey: `${process.env.HONEYCOMB_API_CONFIG_KEY}`, //NOTE: this is supposed to be the "Honeycomb Ingest API key".  I hope this is the same as the main API key I extracted earlier...
   serviceName: 'tracker-crm-fe', // Replace with your application name. Honeycomb uses this string to find your dataset when we receive your data. When no matching dataset exists, we create a new one with this name if your API Key has the appropriate permissions.
   instrumentations: [getWebAutoInstrumentations({
     // Loads custom configuration for xml-http-request instrumentation.
@@ -28,6 +39,8 @@ const sdk = new HoneycombWebSDK({
   })],
 });
 sdk.start();
+
+debugger
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
